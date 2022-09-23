a964fb0af2100e68ebbccb338099b16cdb7611d5
Jonsey <mwjones2186@gmail.com>
2022-09-22 19:45:10 -0700

    modified images and fixed form container

--- src/Componants/ContactForm.js
+++ src/Componants/ContactForm.js
@@ -9,115 +9,144 @@ import Image from "mui-image";
 export default function Form() {
   return (
     <Container>
-     
       <Box
         className="contact-form-container"
-        component="form"
         sx={{
-          m: 2, 
+          m: 2,
           width: 1,
           p: 2,
-          boxShadow:2,
+          boxShadow: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
         noValidate
         autoComplete="off"
       >
         <Image
-            src="https://2e8ram2s1li74atce18qz5y1-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/Bootcamp-MOOC-Learning-Tech-Coding-Programming-Dice.png"
-            height="100"
-            width="100%"
-            fit="fill"
-            showLoading={false}
-            errorIcon={true}
-            shift={null}
-            distance="100px"
-            shiftDuration={900}
-            bgColor="inherit"
-          ></Image>
+          src="https://2e8ram2s1li74atce18qz5y1-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/Bootcamp-MOOC-Learning-Tech-Coding-Programming-Dice.png"
+          height="100"
+          width="100%"
+          fit="fill"
+          showLoading={false}
+          errorIcon={true}
+          shift={null}
+          distance="100px"
+          shiftDuration={900}
+          bgColor="inherit"
+        ></Image>
         {/* <div className="contact-first">
          */}
-          <TextField
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
-            sx={{
-              m:1,
-              width: 1/2,
-              p: 1,
-              boxShadow: 2,
-            }}
-            id="standard-basic"
-            label="First & Last Name"
-            variant="standard"
-          />
-       
-        {/* </div>
+        <Box
+          sx={{
+            m: 2,
+            width: "70%",
+            p: 2,
+            boxShadow: 2,
+            textAlign: "center",
+          }}
+        >
+          <form
+            target="_blank"
+            action="https://formsubmit.co/mwjones2186@gmail.com"
+            method="POST"
+          >
+            <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html" />
+            <TextField
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
+              sx={{
+                width: 1 / 2,
+                boxShadow: 2,
+              }}
+              id="standard-basic"
+              label="Full Name"
+              variant="standard"
+              name="name"
+              required
+            />
+
+            {/* </div>
         <div className="contact-last"> */}
-          <TextField
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width:100%"
-            sx={{
-              m:1,
-              width: 1/2,
-              p: 1,
-              boxShadow: 2,
-            }}
-            id="standard-basic"
-            label="Email Address"
-            variant="standard"
-          />
-        {/* </div>
+            <TextField
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width:100%"
+              sx={{
+                m: 1,
+                width: 1 / 2,
+                p: 1,
+                boxShadow: 2,
+              }}
+              id="standard-basic"
+              label="Email Address"
+              variant="standard"
+              name="email"
+              required
+            />
+            {/* </div>
         <div className="contact-subject"> */}
-          <TextField
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
-            sx={{
-              m:1,
-              width: 3/4,
-              p: 1,
-              boxShadow: 2,
-            }}
-            id="standard-basic"
-            label="Subject"
-            variant="standard"
-          />
-        {/* </div>
+            <TextField
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
+              sx={{
+                m: 1,
+                width: 3 / 4,
+                p: 1,
+                boxShadow: 2,
+              }}
+              id="standard-basic"
+              label="Subject"
+              variant="standard"
+              name="subject"
+              required
+            />
+            {/* </div>
         <div className=""> */}
-          <TextField
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
-            id="standard-multiline-static"
-            sx={{
-              m:1,
-              width: 3/4,
-              height: 200,
-              p: 1,
-              boxShadow: 2,
-            }}
-            
-            label="Message"
-            rows={6}
-            defaultValue=""
-            variant="standard"
-          />
-        {/* </div>
+            <TextField
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
+              id="standard-multiline-static"
+              sx={{
+                m: 1,
+                width: 3 / 4,
+                height: 200,
+                p: 1,
+                boxShadow: 2,
+              }}
+              multiline
+              label="Message"
+              rows={6}
+              defaultValue=""
+              variant="standard"
+              name="message"
+              required
+            />
+            {/* </div>
         <div> */}
-        <Button id="" className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%" 
-        sx={{
-              m:1,
-              width: 1/4,
-              height: 1/2,
-              p: 1,
-              boxShadow: 2,
-            }} variant="outlined" >
-          Send
-        </Button>
-        
+            <Button
+              type="submit"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full width: 100%"
+              sx={{
+                m: 1,
+                width: 1 / 4,
+                height: 1 / 2,
+                p: 1,
+                boxShadow: 2,
+              }}
+              variant="outlined"
+            >
+              Send
+            </Button>
+          </form>
+        </Box>
 
         {/* </div> */}
       </Box>
       <a className="gitHub-btn">
-          <i class="fab fa-github"></i></a>
+        <i className="fab fa-github"></i>
+      </a>
       <a className="linkedIn-btn">
-          <i class="fab fa-linkedin"></i></a>
+        <i className="fab fa-linkedin"></i>
+      </a>
       <a className="slack-btn">
-          <i class="fab fa-slack"></i></a>
-      
+        <i className="fab fa-slack"></i>
+      </a>
     </Container>
   );
 }
